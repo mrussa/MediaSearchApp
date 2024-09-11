589a7d6719196d24c1f7a747ca59384f78a2892c
Maria <rjevskayam@gmail.com>
2024-09-11 16:45:23 +0300

    error correction photo delails screen

--- MediaSearchApp/PhotoDetailViewController.swift
+++ MediaSearchApp/PhotoDetailViewController.swift
@@ -7,6 +7,9 @@ class PhotoDetailViewController: UIViewController {
     var imageView: UIImageView!
     var descriptionLabel: UILabel!
     var authorLabel: UILabel!
+    var activityIndicator: UIActivityIndicatorView!
+    var errorLabel: UILabel!
+    var saveButton: UIButton!
     
     var photo: Photo?
     var loadedImage: UIImage?
@@ -17,53 +20,131 @@ class PhotoDetailViewController: UIViewController {
         
         view.backgroundColor = .white
         
-        // MARK: - UIImageView Setup
-        imageView = UIImageView(frame: CGRect(x: 0, y: 100, width: view.bounds.width, height: view.bounds.height / 2))
-        imageView.contentMode = .scaleAspectFit
-        view.addSubview(imageView)
-        
-        // MARK: - UILabel for Description Setup
-        descriptionLabel = UILabel(frame: CGRect(x: 20, y: imageView.frame.maxY + 20, width: view.bounds.width - 40, height: 100))
-        descriptionLabel.numberOfLines = 0
-        descriptionLabel.font = UIFont.systemFont(ofSize: 16)
-        view.addSubview(descriptionLabel)
-        
-        // MARK: - UILabel for Author Setup
-        authorLabel = UILabel(frame: CGRect(x: 20, y: descriptionLabel.frame.maxY + 10, width: view.bounds.width - 40, height: 30))
-        authorLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        view.addSubview(authorLabel)
+        setupUI()
+        setupActivityIndicator()
+        setupErrorLabel()
         
         // MARK: - Data Loading
         if let photo = photo {
-            descriptionLabel.text = photo.description ?? "Нет описания"
-            authorLabel.text = "Автор: \(photo.author)"
-            
-            if let url = URL(string: photo.urls.regular) {
-                URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let data = data {
-                        DispatchQueue.main.async {
-                            let image = UIImage(data: data)
-                            self.imageView.image = image
-                            self.loadedImage = image
-                        }
-                    }
-                }.resume()
-            }
+            showLoadingState()
+            loadPhotoDetails(photo)
         }
-        
-        setupUI()
     }
     
     // MARK: - UI Setup
     private func setupUI() {
-        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareImage))
-        navigationItem.rightBarButtonItem = shareButton
+        // UIImageView для отображения изображения
+        imageView = UIImageView(frame: CGRect(x: 0, y: 100, width: view.bounds.width, height: view.bounds.height / 2))
+        imageView.contentMode = .scaleAspectFit
+        imageView.isHidden = true
+        view.addSubview(imageView)
         
-        let saveButton = UIButton(type: .system)
+        // UILabel для описания
+        descriptionLabel = UILabel(frame: CGRect(x: 20, y: imageView.frame.maxY + 20, width: view.bounds.width - 40, height: 100))
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.font = UIFont.systemFont(ofSize: 16)
+        descriptionLabel.isHidden = true
+        view.addSubview(descriptionLabel)
+        
+        // UILabel для автора
+        authorLabel = UILabel(frame: CGRect(x: 20, y: descriptionLabel.frame.maxY + 10, width: view.bounds.width - 40, height: 30))
+        authorLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        authorLabel.isHidden = true
+        view.addSubview(authorLabel)
+        
+        // Кнопка сохранения изображения
+        saveButton = UIButton(type: .system)
         saveButton.setTitle("Сохранить изображение", for: .normal)
         saveButton.addTarget(self, action: #selector(saveImageToGallery), for: .touchUpInside)
         saveButton.frame = CGRect(x: 20, y: view.bounds.height - 80, width: view.bounds.width - 40, height: 50)
+        saveButton.isHidden = true
         view.addSubview(saveButton)
+        
+        // Кнопка для поделиться
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareImage))
+        navigationItem.rightBarButtonItem = shareButton
+    }
+    
+    private func setupActivityIndicator() {
+        // Индикатор активности для состояния загрузки
+        activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.center = view.center
+        activityIndicator.hidesWhenStopped = true
+        view.addSubview(activityIndicator)
+    }
+    
+    private func setupErrorLabel() {
+        // UILabel для отображения ошибок
+        errorLabel = UILabel(frame: CGRect(x: 20, y: view.center.y - 20, width: view.bounds.width - 40, height: 40))
+        errorLabel.textColor = .red
+        errorLabel.textAlignment = .center
+        errorLabel.font = UIFont.systemFont(ofSize: 18)
+        errorLabel.isHidden = true
+        view.addSubview(errorLabel)
+    }
+    
+    // MARK: - Data Loading Methods
+    private func loadPhotoDetails(_ photo: Photo) {
+        descriptionLabel.text = photo.description ?? "Нет описания"
+        authorLabel.text = "Автор: \(photo.author)"
+        
+        guard let url = URL(string: photo.urls.regular) else {
+            showErrorState("Неверный URL изображения")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.showErrorState(error.localizedDescription)
+                }
+                return
+            }
+            
+            guard let data = data, let image = UIImage(data: data) else {
+                DispatchQueue.main.async {
+                    self.showErrorState("Не удалось загрузить изображение")
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.loadedImage = image
+                self.imageView.image = image
+                self.showContentState()
+            }
+        }.resume()
+    }
+    
+    // MARK: - UI State Management
+    private func showLoadingState() {
+        activityIndicator.startAnimating()
+        imageView.isHidden = true
+        descriptionLabel.isHidden = true
+        authorLabel.isHidden = true
+        saveButton.isHidden = true
+        errorLabel.isHidden = true
+        navigationItem.rightBarButtonItem?.isEnabled = false
+    }
+    
+    private func showContentState() {
+        activityIndicator.stopAnimating()
+        imageView.isHidden = false
+        descriptionLabel.isHidden = false
+        authorLabel.isHidden = false
+        saveButton.isHidden = false
+        navigationItem.rightBarButtonItem?.isEnabled = true
+    }
+    
+    private func showErrorState(_ message: String) {
+        activityIndicator.stopAnimating()
+        imageView.isHidden = true
+        descriptionLabel.isHidden = true
+        authorLabel.isHidden = true
+        saveButton.isHidden = true
+        errorLabel.text = message
+        errorLabel.isHidden = false
+        navigationItem.rightBarButtonItem?.isEnabled = false
     }
     
     // MARK: - Share Image Action
